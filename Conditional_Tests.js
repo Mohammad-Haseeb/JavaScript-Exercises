824d4a2f98635b524d3273f356e41814c3636b7d
Mohammad Haseeb <mohammad.haseeb310@gmail.com>
2022-10-01 12:05:09 +0500

    23 exercise completed

---
+++ Conditional_Tests.js
@@ -0,0 +1,56 @@
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+let car1 = 'Elantra';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car1 == 'Elantra')
+console.log("Is car != 'Ferrari'? I predict False.")
+
+console.log(car1 == 'Civic')
+
+
+let car2 = 'Civic';
+
+console.log("Is car == 'Civic'? I predict True.")
+
+console.log(car2 == 'Civic')
+
+console.log("Is car == 'Ferrari'? I predict False.")
+
+console.log(car2 == 'Ferrari')
+
+let car3 = 'Ferrari';
+
+console.log("Is car == 'Ferrari'? I predict True.")
+
+console.log(car3 == 'Ferrari')
+console.log("Is car2 == 'Civic'? I predict False.")
+
+console.log(car2 == 'Civic')
+
+
+let car4 = 'BMW';
+
+console.log("Is car == 'BMW'? I predict True.")
+
+console.log(car4 == 'BMW')
+console.log("Is car4 == 'MNW? I predict False.")
+
+console.log(car2 == car4)
+
+
+
+let car5 = 'Mercedez';
+
+console.log("Is car == 'Mercedez'? I predict True.")
+
+console.log(car5 == 'Mercedez')
+
+console.log("Is car4 == 'Mercedez'? I predict False.")
+
+console.log(car4 == car5)
