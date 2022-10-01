dba59848a26d4157200a6e447ab6e505adac165d
Mohammad Haseeb <mohammad.haseeb310@gmail.com>
2022-10-01 11:27:08 +0500

    17 exercise completed

--- Shrinking_Guest_List.js
+++ Shrinking_Guest_List.js
@@ -30,3 +30,25 @@ for(const name of names){
   for(const name of names){
     console.log(`${message}  ${name} at 8pm on 6-10-2022`)
   }
+
+  console.log('\n I can invite only two people \n')
+
+//   you’re sorry you can’t invite them to dinner
+
+for(let i =0;i<names.length+2;++i){
+    console.log(`sorry I can’t invite them to dinner ${names.pop()}`)
+    
+  }
+
+  console.log("\n New Guests with more tables \n")
+  for(const name of names){
+    console.log(`You're still invited ${name} at 8pm on 6-10-2022`)
+  }
+  names.pop()
+  names.pop()
+
+  console.log(" \n check empty array\n")
+
+  for(const name of names){
+    console.log(`You're still invited ${name} at 8pm on 6-10-2022`)
+  }
